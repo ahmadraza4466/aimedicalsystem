fe5dd9ef123e0497c24fb0800cff2a9e4dd0f180
Muhammad Sharyar <msharyar45@gmail.com>
2023-12-31 02:13:59 +0500

    fixed home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-"use client";
-import { refreshAccessToken } from "@/actions/auth";
 import { redirect } from "next/navigation";
-import { useEffect, useRef } from "react";
 
 export default async function Home() {
   redirect("/chats");
